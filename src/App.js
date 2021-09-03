b1b6345633bbbf749f2a77373ee5421e364fb2f6
taaalha <talha.binkhalid@aalto.fi>
2021-09-03 13:03:05 +0300

    d_complex state

--- src/App.js
+++ src/App.js
@@ -1,66 +1,33 @@
 import React, {useState} from 'react'
 
-const Hello = ({name,age}) => {
-
-	const bornYear = () => new Date().getFullYear()-age
-
-	return (
-	  <div>
-		<p>
-		  Hello {name}, you are {age} years old
-		</p>
-		<p> So you were probably born in {bornYear()} </p>
-	  </div>
-	)
-  }
-  
-const Display = ({counter}) => <div>{counter}</div>
-
-
-
-
-const Button = ({ onClick, text}) =>  (
-
-		<button onClick={onClick} >
-		{text}
-		</button>
-
-	)
-
-
-
-
   const App = () => {
-const [counter, setCounter ] = useState(0)
+const [clicks, setClicks ] = useState(
+	{
+		left: 0, right : 0
+	}
+)
 
-const increaseByOne = () => setCounter(counter+1)
+const handleLeftClick = () => {
 
-const setToZero = () => setCounter(0)
+	setClicks({... clicks, left : clicks.left + 1})
+}
 
-const decreaseByOne = () => setCounter(counter-1)
+const handleRightClick = () => {
+
+    setClicks({... clicks, right: clicks.right + 1})
+  }	
 
 
-console.log ('rendering ....', counter)
 return (
 <div>
-	<Display counter = {counter} />
+	{clicks.left}
 
-	<Button 
-	onClick = {increaseByOne}
-	text = 'plus'
-	/>
+	<button onClick = {handleLeftClick} > left </button>
 
-	<Button 
-	onClick = {setToZero}
-	text = 'zero'
-	/>
-
-	<Button 
-	onClick = {decreaseByOne}
-	text = 'minus'
-	/>
+	<button onClick={handleRightClick}>right</button>
 
 
+	{clicks.right}
 </div>
 )
   }
