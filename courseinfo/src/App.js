edb8da45828ab34cfc7d7b8326377fb36ad78ae0
taaalha <talha.binkhalid@aalto.fi>
2021-09-13 16:59:47 +0300

    exercise 1.5

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 const Header = (props) => {
 	return (
 	<div>
-		<h1> {props.course} </h1>
+		<h1> {props.course.name} </h1>
 	</div>	
 	)
 }
 const Content = (props) => { 	console.log(props)
 	return (
-
+	
 	<div>
 		<Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
 		<Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
@@ -37,32 +37,33 @@ const Total = (props) => {
 
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const parts = [
-	  {
-		name: 'Fundamentals of React',
-		exercises: 10
-	  },
-	  {
-		name: 'Using props to pass data',
-		exercises: 7
-	  },
-	  {
-		name: 'State of a component',
-		exercises: 14
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+		  {
+			name: 'Fundamentals of React',
+			exercises: 10
+		  },
+		  {
+			name: 'Using props to pass data',
+			exercises: 7
+		  },
+		  {
+			name: 'State of a component',
+			exercises: 14
+		  }
+		]
 	  }
-	]
 
 
 
-	
   return (
     <div >
       <Header course={course} />
 
-	  <Content parts={parts}/>
+	  <Content parts={course.parts}/>
 
-	  <Total parts={parts} />
+	  <Total parts={course.parts} />
 
     </div>
   )
